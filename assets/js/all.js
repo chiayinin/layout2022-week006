47db65b78807e44284f3afde8d9b288a4faf3f43
chiayinin <chiayinin@gmail.com>
2022-09-13 23:06:10 +0800

    Update 2022-09-13T15:06:04.794Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,30 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// swiper
+var swiper = new Swiper(".pgLgSwiper", {
+  // 自動輪播
+  // autoplay: {
+  //   disableOnInteraction: false,
+  //   delay: 0,
+  // },
+  loop: false,
+  speed: 3000,
+  spaceBetween: 16,
+  slidesPerView: 3,
+  observer: true,
+  observerParents: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  // RWD
+  breakpoints: {
+    380: {
+      slidesPerView: 1
+    },
+    480: {
+      slidesPerView: 1
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
